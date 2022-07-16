4352a224011744761775f9c6513f10d4d3dd2189
Juliano Boese <juliano.boese@gmail.com>
2022-07-16 14:17:04 -0300

    feat: :lipstick: apply xp logo and theme

---
+++ src/utils/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ffc000',
+    },
+    secondary: {
+      main: '#ffc000',
+    },
+  },
+});
+
+export default theme;
