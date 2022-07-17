e98669d72910638b5ecb24b742968af833953926
Juliano Boese <juliano.boese@gmail.com>
2022-07-16 21:37:39 -0300

    feat(register): :sparkles: replace template with real data from project

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,37 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory, Link as RouterLink } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { LoadingButton } from '@mui/lab';
-import { useHistory } from 'react-router-dom';
 import logoXp from '../assets/logo-xp.png';
 import register from '../services/register';
 import getUser from '../services/user';
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 export default function Register() {
   const [firstName, setFirstName] = useState('');
@@ -98,7 +78,7 @@ export default function Register() {
                 required
                 fullWidth
                 id="firstName"
-                label="First Name"
+                label="Nome"
                 autoFocus
                 onChange={(event) => setFirstName(event.target.value)}
               />
@@ -108,7 +88,7 @@ export default function Register() {
                 required
                 fullWidth
                 id="lastName"
-                label="Last Name"
+                label="Sobrenome"
                 name="lastName"
                 autoComplete="family-name"
                 onChange={(event) => setLastName(event.target.value)}
@@ -119,7 +99,7 @@ export default function Register() {
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                label="E-mail"
                 name="email"
                 autoComplete="email"
                 onChange={(event) => setEmail(event.target.value)}
@@ -130,17 +110,11 @@ export default function Register() {
                 required
                 fullWidth
                 name="password"
-                label="Password"
+                label="Senha"
                 type="password"
                 id="password"
                 autoComplete="new-password"
                 onChange={(event) => setPassword(event.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
               />
             </Grid>
           </Grid>
@@ -160,19 +134,29 @@ export default function Register() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Abrir conta
             </Button>
           )}
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+              <RouterLink to='/'>
+                <Link variant="body2">
+                  Já possui uma conta? Acesse.
+                </Link>
+              </RouterLink>
             </Grid>
           </Grid>
         </Box>
       </Box>
-      <Copyright sx={{ mt: 5 }} />
+      <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
+        {'Projeto desenvolvido para o processo seletivo da XP Inc.'}
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 1 }}>
+        <Link color="inherit" href="https://github.com/julianoboese">
+          Juliano Boese
+        </Link>
+          ,{' '} 2022
+      </Typography>
     </Container>
   );
 }
