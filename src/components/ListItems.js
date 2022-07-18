075fa29fc4b3688a30e5caa3b59c32c9bc014ce5
Juliano Boese <juliano.boese@gmail.com>
2022-07-18 14:44:31 -0300

    refactor(dash): :fire: remove unused menu items

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -2,11 +2,7 @@ import React, { useContext } from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
+import PaidIcon from '@mui/icons-material/Paid';
 import { useHistory } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import { getBalance } from '../services/account';
@@ -31,29 +27,11 @@ export default function ListItems() {
 
   return (
     <>
-      <ListItemButton onClick={() => setIsActionOpen(true)}>
-        <ListItemIcon>
-          <ShoppingCartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Investir" />
-      </ListItemButton>
       <ListItemButton onClick={openAccount}>
         <ListItemIcon>
-          <PeopleIcon />
+          <PaidIcon />
         </ListItemIcon>
         <ListItemText primary="Conta digital" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary="Reports" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <LayersIcon />
-        </ListItemIcon>
-        <ListItemText primary="Integrations" />
       </ListItemButton>
     </>
   );
