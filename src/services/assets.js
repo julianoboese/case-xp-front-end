2a02e476be44e31b5e95c006b29d5903cbed0f4a
Juliano Boese <juliano.boese@gmail.com>
2022-07-18 11:02:12 -0300

    feat(assets): :sparkles: add function to get all assets from backend

--- src/services/assets.js
+++ src/services/assets.js
@@ -1,3 +1,19 @@
+export const getAllAssets = async () => {
+  const token = sessionStorage.getItem('token');
+  const response = await fetch('https://case-xp-back-end.herokuapp.com/ativos/all', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  });
+
+  if (response.status >= 400) return { error: { status: response.status } };
+
+  const responseJson = await response.json();
+  return responseJson;
+};
+
 export const getAssets = async () => {
   const token = sessionStorage.getItem('token');
   const response = await fetch('https://case-xp-back-end.herokuapp.com/ativos', {
