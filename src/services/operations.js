cf483b9b7888b9b9ad4d22d55b7d68faad69a30d
Juliano Boese <juliano.boese@gmail.com>
2022-07-22 18:39:35 -0300

    feat(services): :sparkles: create function to request operations from back-end

---
+++ src/services/operations.js
@@ -0,0 +1,23 @@
+const getOperations = async () => {
+  const token = sessionStorage.getItem('token');
+  const response = await fetch(`https://${process.env.REACT_APP_BACK_ENV}.herokuapp.com/operations`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  });
+
+  const responseJson = await response.json();
+
+  if (response.status >= 400) {
+    return {
+      status: response.status,
+      message: responseJson.message,
+    };
+  }
+
+  return responseJson;
+};
+
+export default getOperations;
